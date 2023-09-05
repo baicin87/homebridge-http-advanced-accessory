f3169943f6f41078748e6c24f0d3b8879f84d51b
baicin87 <55091346+baicin87@users.noreply.github.com>
2023-09-05 23:34:18 +0200

    adaptive lighting controller restricted to ColorTemperature
    only bulbs with ColorTemperature can effectively use adaptive lighting controller

--- index.js
+++ index.js
@@ -300,7 +300,7 @@ HttpAdvancedAccessory.prototype = {
 		
 		var newService = new Service[this.service](this.name);
 		// create adaptive lighting controller for light bulbs
-		if (this.service == "Lightbulb") {
+		if (this.service == "Lightbulb" and this.optionCharacteristic.contains("ColorTemperature")) {
 			this.adaptiveLightingController = new AdaptiveLightingController(newService);
 		}
 
