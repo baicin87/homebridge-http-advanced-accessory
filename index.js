3b640984b62ef9f3c0bb20d67b64cc757a76a23e
baicin87 <55091346+baicin87@users.noreply.github.com>
2023-09-05 23:36:14 +0200

    forgot its not python

--- index.js
+++ index.js
@@ -300,7 +300,7 @@ HttpAdvancedAccessory.prototype = {
 		
 		var newService = new Service[this.service](this.name);
 		// create adaptive lighting controller for light bulbs
-		if (this.service == "Lightbulb" and this.optionCharacteristic.contains("ColorTemperature")) {
+		if (this.service == "Lightbulb" && this.optionCharacteristic.contains("ColorTemperature")) {
 			this.adaptiveLightingController = new AdaptiveLightingController(newService);
 		}
 
